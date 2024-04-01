f062b32ce227af1fd82a1596d7332700bff342bc
laidback_tay <teqwon.norman@outlook.com>
2024-03-31 23:08:24 -0400

    started the route guide client

---
+++ route_guide_client.py
@@ -0,0 +1,27 @@
+
+def make_route_note():
+    pass
+
+def guide_get_one_feature():
+    pass
+
+def guide_get_feature():
+    pass
+
+def guide_list_features():
+    pass
+
+def generate_route():
+    pass
+
+def guide_record_route():
+    pass
+
+def generate_messages():
+    pass
+
+def guide_route_chat():
+    pass
+
+def run():
+    pass
